fbe42828cc995eb2f2db258604d45dea3a001dd3
zhuwm <zhuwm@cairenhui.com>
2016-04-08 15:07:54 +0800

    add redis

--- h5test/src/main/java/com/zhuwm/redis/OnLineUserImpl.java
+++ h5test/src/main/java/com/zhuwm/redis/OnLineUserImpl.java
@@ -1,7 +1,33 @@
 package com.zhuwm.redis;
 
+import com.zhuwm.redis.jedis.JedisUtil;
+
+import redis.clients.jedis.Jedis;
+
 public class OnLineUserImpl {
 	
+	
+	private static String B_S_ONLINEUSERQUENE_NAME="k_ol_u_name";
+	
+	private Jedis jedis =JedisUtil.getJedis();
+	
+	public void putUserToQueue(String userId){
+		jedis.lrem(OnLineUserImpl.B_S_ONLINEUSERQUENE_NAME, 0, userId);
+		jedis.lpush(OnLineUserImpl.B_S_ONLINEUSERQUENE_NAME, userId);
+	}
+	
+	public Long getQueueCount(){
+		return jedis.llen(OnLineUserImpl.B_S_ONLINEUSERQUENE_NAME);
+	}
+	
+	/**
+	 * 返回最后一个队列
+	 * @author zhuweiming
+	 * @return
+	 */
+	public static String getLastUserFromQueue(){
+		return "";
+	}
 	
 
 }
---
+++ h5test/src/main/java/com/zhuwm/redis/VideoQueueImpl.java
@@ -0,0 +1,14 @@
+package com.zhuwm.redis;
+
+/**
+ * 封装等待进行视频见证的用户队列。队列在redis中存放。
+ * 调用者不需要关心redis实现机制。
+ * 
+ * 功能说明: <br>
+ * 系统版本: v1.0<br>
+ * 开发人员: @author zhuweiming<br>
+ * 开发时间: 2016年4月7日<br>
+ */
+public class VideoQueueImpl {
+
+}
