f6e9acacc08c7d3e2a011de8405b43057d389bcc
zhuwm <zhuwm@cairenhui.com>
2016-04-28 15:01:13 +0800

    commit

--- h5test/src/main/java/com/zhuwm/h5/controller/WebSocketController.java
+++ h5test/src/main/java/com/zhuwm/h5/controller/WebSocketController.java
@@ -1,15 +1,19 @@
 package com.zhuwm.h5.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.zhuwm.util.CookieUtil;
 
 /*
  * ======
@@ -20,8 +24,16 @@ public class WebSocketController  extends DispatcherServlet {
 
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * 处理登录的ajax方法，返回json数据
+	 * @author zhuweiming
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@ResponseBody  
 	@RequestMapping(value = "/websocketlogin.do")
-	public ModelAndView websocketLogin(HttpServletRequest request, HttpServletResponse response) {
+	public List<String> websocketLogin(HttpServletRequest request, HttpServletResponse response) {
 
 		
 		//首先取cookid中的userId
@@ -35,13 +47,12 @@ public class WebSocketController  extends DispatcherServlet {
 			}
 		}
 		
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("websocket/index");
+		ArrayList<String> resultList= new ArrayList<String>();
 		
 		//如果cookie中有保存userId，则已登录
 		if(userId!=null){
-			mav.addObject("userId",userId);
-			return mav;
+			resultList.add(userId);
+			return resultList;
 		}
 		
 		//没有userId，则未登录。从request中取值来判断
@@ -55,18 +66,16 @@ public class WebSocketController  extends DispatcherServlet {
 			loginUserId="";
 		}
 
-		mav.addObject("userId",loginUserId);
-		
-		return mav;
+		resultList.add(loginUserId);
+		return resultList;
 	}
 	
 	
 	
 	
-	@RequestMapping(value = "/websocket.do")
+	@RequestMapping(value = "/websocketindex.do")
 	public ModelAndView websocketIndex() {
-
-		
+				
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("websocket/index");
 		return mav;
